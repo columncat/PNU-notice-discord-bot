729f41bb18ffd431751bb25c09dc027784d730b2
columncat <127417901+columncat@users.noreply.github.com>
2025-02-20 16:11:01 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import time
 import discord
 from discord.ext import commands
 from app_private import token
+from app_private import mec_channel
+from app_private import test_channel
 import os.path
 import csv
 import mec_notice
@@ -17,13 +20,13 @@ QUEUE_FILE = "push_queue.csv"
 
 
 def __read_csv(path):
-    if not os.path.isfile(path):
-        return []
     content = []
+    if not os.path.isfile(path):
+        return content
     with open(path, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            if row.__len__() == 3:
+            if len(row) == 3:
                 content.append(row)
     return content
 
@@ -39,26 +42,51 @@ def update_all():
     main_schedule.update()
 
 
-def push():
-    updates = __read_csv(QUEUE_FILE)
-    if updates.__len__() > 0:
-        @bot.event
-        async def on_ready():
-            print(f'Login bot: {bot.user}')
-            for (id, title, url) in updates:
-                channel = bot.get_channel((int)(id))
-                if (url != 'empty'):
-                    embed = discord.Embed()
-                    embed.description = f'[{title}]({url})'
-                    await channel.send(embed=embed)
-                else:
-                    await channel.send(title)
-            open(QUEUE_FILE, "w")
-            exit()
-
-        bot.run(token)
+@bot.event
+async def on_ready():
+    print(f'Login bot: {bot.user}')
+    localtime = time.localtime().tm_min
+    prev_time = localtime
+    update_interval = 5
+    try:
+        channel = bot.get_channel(test_channel)
+        await channel.send('initializing bot...')
+        update_all()
+        await channel.send('bot successfully initialized.')
+        while True:
+            # only trigger update every 5 minutes
+            localtime = time.localtime().tm_min
+            if localtime % update_interval == 0 and localtime != prev_time:
+                prev_time = localtime
+                
+                # get updated
+                update_all()
+                updates = __read_csv(QUEUE_FILE)
+                
+                # send messages
+                if updates:
+                    for (id, title, url) in updates:
+                        channel = bot.get_channel(int(id))
+                        if (url != 'empty'):
+                            embed = discord.Embed()
+                            embed.description = f"[{title}]({url})"
+                            if (int(id)) == mec_channel:
+                                await channel.send(f"@everyone {title}", embed=embed)
+                            else:
+                                await channel.send(title, embed=embed)
+                        else:
+                            await channel.send(title)
+                    
+                    open(QUEUE_FILE, "w")   # flush queue
+                    channel = bot.get_channel(test_channel)
+                    await channel.send(f'{len(updates)} messages sent.')
+            else:
+                time.sleep(20)
+    finally:
+        channel = bot.get_channel(test_channel)
+        await channel.send('@everyone bot shutting down due to internal error...')
+        exit(1)
 
 
 if __name__ == "__main__":
-    update_all()
-    push()
+    bot.run(token)
